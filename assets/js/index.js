37cbf0baea8821daf1771ec3d0de304d3730fd49
Asa Smith <smith.asa.la@gmail.com>
2017-10-24 13:37:56 -0400

    add countdown timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,6 +25,59 @@ const checkScreenWidth = function () {
 
 checkScreenWidth()
 
+const countdown = () => {
+
+// set kickoff date  
+  const kickoff = new Date(2018, 9, 1)
+
+// create variables
+  let seconds  
+  let minutes  
+  let hours  
+  let days  
+
+// convert kickoff date to ms, subtract current time from future date
+  const compareDate = () => {
+    let currentTime = new Date().getTime()
+    let kickoffInMS = kickoff.getTime()
+    return kickoffInMS - currentTime
+  }
+
+// convert difference of future date/current time into units
+  const convertTime = () => {
+    seconds = Math.floor(compareDate() / 1000)
+    minutes = Math.floor(seconds / 60)
+    hours = Math.floor(minutes / 60)
+    days = Math.floor(hours / 24)
+    
+    hours %= 24
+    minutes %= 60
+    seconds %= 60
+  }
+
+// display timer
+  const countdownDisplay = () => {
+    convertTime()
+    
+    let daysDisp = document.querySelector('#days')
+    let hoursDisp = document.querySelector('#hours')
+    let minDisp = document.querySelector('#minutes')
+    let secDisp = document.querySelector('#seconds')
+    
+    daysDisp.innerHTML = `${days}:`
+    hoursDisp.innerHTML = `${hours}:`
+    minDisp.innerHTML = `${minutes}:`
+    secDisp.innerHTML = `${seconds}`
+  }
+// run timer display function every second
+  const timer = window.setInterval(countdownDisplay, 1000)
+
+// call timer function
+  timer()
+}
+
+countdown()
+
 // show nav menu when hamburger is clicked
 
 const hamburger = document.querySelector('#menu-toggle input')

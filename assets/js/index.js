6fb064f5bb6173e4174d22ab329476ee2bcf3a7e
Asa Smith <asa.smith@berndtgroup.net>
2018-07-15 09:38:40 -0400

    remove-counter
    - remove countdown js and css
    - add top margin to fan-exp section

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,65 +21,6 @@ const checkScreenWidth = function () {
 
 checkScreenWidth()
 
-const countdown = () => {
-  const body = document.querySelector('body')
-
-  if (body.getAttribute('id') === 'index') {
-
-// set kickoff date  
-  const kickoff = new Date(2018, 6, 14, 19) // sat july 14 7:00pm
-
-// create variables
-  let seconds  
-  let minutes  
-  let hours  
-  let days  
-
-// convert kickoff date to ms, subtract current time from future date
-  const compareDate = () => {
-    let currentTime = new Date().getTime()
-    let kickoffInMS = kickoff.getTime()
-    return kickoffInMS - currentTime
-  }
-
-// convert difference of future date/current time into units
-  const convertTime = () => {
-    seconds = Math.floor(compareDate() / 1000)
-    minutes = Math.floor(seconds / 60)
-    hours = Math.floor(minutes / 60)
-    days = Math.floor(hours / 24)
-    
-    hours %= 24
-    minutes %= 60
-    seconds %= 60
-  }
-
-// display timer
-  const countdownDisplay = () => {
-    convertTime()
-    
-    let daysDisp = document.querySelector('#days')
-    let hoursDisp = document.querySelector('#hours')
-    let minDisp = document.querySelector('#minutes')
-    let secDisp = document.querySelector('#seconds')
-    
-    daysDisp.innerHTML = `${days}:`
-    hoursDisp.innerHTML = `${hours}:`
-    minDisp.innerHTML = `${minutes}:`
-    if (seconds < 10) {
-      secDisp.innerHTML = `0${seconds}`
-    } else {
-    secDisp.innerHTML = `${seconds}`
-      }
-  }
-// run timer display function every second
- window.setInterval(countdownDisplay, 1000)
-
-  }
-}
-
-countdown()
-
 // show nav menu when hamburger is clicked
 
 const hamburger = document.querySelector('#menu-toggle input')
@@ -118,7 +59,7 @@ function debounce (func, wait = 10, immediate = true) {
 
 function headerColorChange () {
   const header = document.getElementById('top-nav')
-  
+
   const addClassAt = (window.scrollY + window.innerHeight) - header.clientHeight
   const divBottom = header.offsetTop + header.clientHeight
   const isScrolledPast = window.scrollY > divBottom
@@ -139,9 +80,9 @@ const constructionCam = () => {
   const body = document.querySelector('body')
 
   if (body.getAttribute('id') === 'construction-cam') {
-    console.log('this is the con cam page') 
+    console.log('this is the con cam page')
 
-// set timestamp to current time on page load 
+// set timestamp to current time on page load
   const img = document.getElementById('cameraImage')
   const ts = new Date().getTime();
   img.src = `https://oxblue.com/archive/a9e49108373fed879685338c5bbf0cc6/800x600.jpg?ts=${ts}`
@@ -154,4 +95,4 @@ const constructionCam = () => {
   },90000);
   }
 }
-constructionCam() 
+constructionCam()
